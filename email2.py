9a9a1755e7087a5a7f232fc2678118ddd95afe67
Shreyas-Gupta-21 <62789950+Shreyas-Gupta-21@users.noreply.github.com>
2021-05-29 23:26:58 +0530

    Add files via upload

---
+++ email2.py
@@ -0,0 +1,24 @@
+import csv
+import ssl
+import smtplib
+
+import email.utils
+
+message = """Subject: Your grade
+
+Hi {name}, your grade is {grade}"""
+from_address = "shreyasgup21@gmail.com"
+password = input("Type your password and press enter: ")
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(from_address, password)
+    with open("ntacts_file.csv") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row
+        for name, email, grade in reader:
+            server.sendmail(
+                from_address,
+                email,
+                message.format(name=name,grade=grade),
+            )
